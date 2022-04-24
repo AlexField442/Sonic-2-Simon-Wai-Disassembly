dc96be9f3aecc1a00c85ac6eb38edbe8e8c95100
AlexField290 <alexfield290@gmail.com>
2022-04-24 12:24:01 +1000

    Relabeling and Obj01
    Replaced *ALL* "Offset_00XXXXX" labels with "loc_XXXXX" labels. and begun documenting Obj01 (Sonic in levels).

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-asm68k /k /p /o ae-,c+ Sonic2B.asm, s2built.bin >s2.log, , s2.lst
+asm68k /k /p /o ae-,c+ s2b.asm, s2built.bin >s2.log, , s2.lst
---
+++ s2b.constants.asm
@@ -0,0 +1,3 @@
+Sonic_top_speed:		equ $FFFFF760
+Sonic_acceleration:		equ $FFFFF762
+Sonic_deceleration:		equ $FFFFF764
